8f3b9606a9fbd11a7bc981ed0d8c13a103056ff0
mscastillop <manuelsantiagocastillopalacio@gmail.com>
2022-11-06 00:58:28 -0500

    Origin Commit

---
+++ src/test/java/co/com/choucair/certification/proyectobase/runners/RunnerTags.java
@@ -0,0 +1,23 @@
+package co.com.choucair.certification.proyectobase.runners;
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/academyChoucair.feature",
+        tags = "@stories",
+        glue = "co.com.choucair.certification.proyectobase.stepdefinitions",
+        snippets = SnippetType.CAMELCASE)
+
+
+public class RunnerTags {
+
+
+}
